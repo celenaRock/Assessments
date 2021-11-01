041b221847175ba7a7f5750a6710de554478801b
celenaRock <92170269+celenaRock@users.noreply.github.com>
2021-10-31 22:28:19 -0500

    Add files via upload
    Final commit assessment 2

---
+++ web/profile.js
@@ -0,0 +1,17 @@
+const color = document.getElementById("color");
+
+const place = document.getElementById("place");
+
+const ritual = document.getElementById("ritual");
+
+color.addEventListener("mouseover", function() {
+    alert("red")
+})
+
+place.addEventListener("mouseover", function() {
+    alert("home")
+})
+
+ritual.addEventListener("mouseover", function() {
+    alert("make coffee")
+})
