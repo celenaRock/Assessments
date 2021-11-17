d3092dd92478a69b5c7f6499b246ac83b43e8e1a
Celena Rock <92170269+celenaRock@users.noreply.github.com>
2021-11-17 13:52:20 -0600

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+
+
+  res.status(200).send(randomCompliment);
+  
+});
+let users = [user1, user2, users]
+
+app.delete('/api/users/:username', (req, res) => {
+  let existingUsername  = req.params.username
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === existingUsername) {
+      users.splice(i, 1)
+      res.status(200).send("User deleted.")
+      return
+    }
+  }
+  res.status(400).send("User not found.")
+})
+
+const userController = require('./controllers/userController')
+app.get('/api/users', userController.getAllUsers)
+app.post('/api/users', userController.createUser)
+app.put('/api/users/:username', userController.updateUser)
+app.delete('/api/users/:username', userController.deleteUser)
+
+app.listen(4000, () => console.log("Server running on 4000"));
